9555cffd01019231a69cf39d79d107f29d1ffc45
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-02-25 22:16:28 -0300

    finally main layout screen

--- employeeProject/src/br/edu/employee/view/MainForm.java
+++ employeeProject/src/br/edu/employee/view/MainForm.java
@@ -1,5 +1,8 @@
 package br.edu.employee.view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,16 +19,40 @@ public class MainForm extends JFrame {
 	}
 	
 	public void toStart() {
-				
-		btnNew.setBounds(100, 300, 70, 20);
-		txtEmployeeLabel.setBounds(100, 20, 100, 20);
+		
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gbc = new GridBagConstraints();
 		
 		
-		getContentPane().add(btnNew);
-		getContentPane().add(txtEmployeeLabel);
-		getContentPane().add(txtEmployeeList);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.0;
+		add(txtEmployeeLabel, gbc);
+		
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		add(txtEmployeeList, gbc);
+		
+		gbc.gridy = 2;
+		gbc.gridwidth = 1;
+		gbc.fill = GridBagConstraints.CENTER;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		add(btnNew, gbc);
+		
+		
+		
 		setSize(500, 350);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		
 	}
 }
